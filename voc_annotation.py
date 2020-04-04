16dccaf15067a1ba9dd0c3629c5e3a275a048ed5
KaGen1999 <1032336124@qq.com>
2020-04-04 16:34:43 +0800

    update the files

--- voc_annotation.py
+++ voc_annotation.py
@@ -3,7 +3,7 @@ from os import getcwd
 
 sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["person"]
 
 
 def convert_annotation(year, image_id, list_file):
